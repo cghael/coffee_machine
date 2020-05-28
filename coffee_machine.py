0701f2e08f4e7555d06774c5f59db72bab50d664
cghael <55437891+cghael@users.noreply.github.com>
2020-05-28 15:38:07 +0300

    continue latter at home

--- coffee_machine.py
+++ coffee_machine.py
@@ -6,18 +6,25 @@ def status_inf(water, milk, gr, cups):
     print(cups, "of disposable cups")
 
 
+def find_min_cups(water, milk, gr, cups):
+    min_cups = water // 200
+    if min_cups > milk // 50:
+        min_cups = milk // 50
+    if min_cups > gr // 15:
+        min_cups = gr // 15
+    if min_cups > cups:
+        min_cups = cups
+    return min_cups
+
+
 water = int(input("Write how many ml of water the coffee machine has: "))
 milk = int(input("Write how many ml of milk the coffee machine has: "))
 gr = int(input("Write how many grams of coffee beans the coffee machine has: "))
 cups = int(input("Write how many cups of coffee you will need: "))
 
 status_inf(water, milk, gr, cups)
+min_cups = find_min_cups(water, milk, gr, cups)
 
-min_cups = water // 200
-if min_cups > milk // 50:
-    min_cups = milk // 50
-if min_cups > gr // 15:
-    min_cups = gr // 15
 if min_cups < cups:
     print("No, I can make only", min_cups, "cups of coffee")
 elif min_cups == cups:
